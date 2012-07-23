17df6664ab6c6f911445ed5b3af9aa7096324bd3
Peeter Marvet <pets@tehnokratt.net>
2012-07-23 18:15:20 +0300

    established the repo
    Signed-off-by: Peeter Marvet <pets@tehnokratt.net>

---
+++ template-page-test.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template Name: TEST TEST
+ *
+ * This Full Width template removes the primary and secondary asides so that content
+ * can be displayed the entire width of the #content area.
+ * 
+ * @package Thematic
+ * @subpackage Templates
+ */
+
+ wp_page_menu();
+ echo wp_nav_menu(thematic_nav_menu_args());
+
+?>
